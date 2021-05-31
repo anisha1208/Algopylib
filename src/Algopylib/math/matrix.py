0fb32ca123c06fb7c4fd9122288b671b744c7bea
Shivam Saxena <76241677+shivam-s16@users.noreply.github.com>
2021-05-31 22:18:20 +0530

    matrix module

--- src/Algopylib/math/matrix.py
+++ src/Algopylib/math/matrix.py
@@ -41,3 +41,24 @@ def inverse_of_matrix_2(matrix: list[list[float]]) -> list[list[float]]:
     # Calculate the inverse of the matrix
     return [[float(D(n) / determinant) or 0.0 for n in row] for row in swapped_matrix]
 
+def inverse_of_matrix_3(matrix: list[list[float]]) -> list[list[float]]:
+    
+    invDet = 1 / det3;
+     D = Decimal  # An abbreviation to be conciseness
+    # Calculate the determinant of the matrix
+    determinant = D(det3(matrix))
+    if determinant == 0:
+        raise ValueError("This matrix has no inverse.")
+    temp = [[0.0, 0.0,0.0],[0.0, 0.0,0.0], [0.0, 0.0,0.0]]
+
+    temp[0][0] = (matrix[1][1] * matrix[2][2] - matrix[2][1] * matrix[1][2]) * invDet
+    temp[0][1] = (matrix[0][2] * matrix[2][1] - matrix[0][1] * matrix[2][2]) * invDet
+    temp[0][2] = (matrix[0][1] * matrix[1][2] - matrix[0][2] * matrix[1][1]) * invDet
+    temp[1][0] = (matrix[1][2] * matrix[2][0] - matrix[1][0] * matrix[2][2]) * invDet
+    temp[1][1] = (matrix[0][0] * matrix[2][2] - matrix[0][2] * matrix[2][0]) * invDet
+    temp[1][2] = (matrix[1][0] * matrix[0][2] - matrix[0][0] * matrix[1][2]) * invDet
+    temp[2][0] = (matrix[1][0] * matrix[2][1] - matrix[2][0] * matrix[1][1]) * invDet
+    temp[2][1] = (matrix[2][0] * matrix[0][1] - matrix[0][0] * matrix[2][1]) * invDet
+    temp[2][2] = (matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]) * invDet
+
+    return temp
