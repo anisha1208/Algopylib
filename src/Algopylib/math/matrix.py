829121e853485fee468a3f6b7e6b42e61c6e1d6c
Shivam Saxena <76241677+shivam-s16@users.noreply.github.com>
2021-05-31 10:06:24 +0530

    matrix module

--- src/Algopylib/math/matrix.py
+++ src/Algopylib/math/matrix.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
 from decimal import Decimal
-
+def det2(matrix: list[list[float]]) -> float:
+    ''' Determinant of a 2x2 matrix. '''
+    return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]
 
 def inverse_of_matrix(matrix: list[list[float]]) -> list[list[float]]:
     """
@@ -24,7 +26,7 @@ def inverse_of_matrix(matrix: list[list[float]]) -> list[list[float]]:
 
     D = Decimal  # An abbreviation to be conciseness
     # Calculate the determinant of the matrix
-    determinant = D(matrix[0][0]) * D(matrix[1][1]) - D(matrix[1][0]) * D(matrix[0][1])
+    determinant = D(det2(matrix))
     if determinant == 0:
         raise ValueError("This matrix has no inverse.")
     # Creates a copy of the matrix with swapped positions of the elements
@@ -33,4 +35,4 @@ def inverse_of_matrix(matrix: list[list[float]]) -> list[list[float]]:
     swapped_matrix[1][0], swapped_matrix[0][1] = -matrix[1][0], -matrix[0][1]
     # Calculate the inverse of the matrix
     return [[float(D(n) / determinant) or 0.0 for n in row] for row in swapped_matrix]
-print(inverse_of_matrix([[2.5, 5], [1, 2]]))
+
