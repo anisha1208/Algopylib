0b5ccd849bb5ccdb09b805c37026b26253c05416
Shivam Saxena <76241677+shivam-s16@users.noreply.github.com>
2021-06-01 20:57:07 +0530

    Update stat.py

--- src/Algopylib/math/stat.py
+++ src/Algopylib/math/stat.py
@@ -705,209 +705,29 @@ def covariance(x, y, /):
     sxy = fsum((xi - xbar) * (yi - ybar) for xi, yi in zip(x, y))
     return sxy / (n - 1)
 
-
-
-
-class NormalDist:
-    "Normal distribution of a random variable"
-    # https://en.wikipedia.org/wiki/Normal_distribution
-    # https://en.wikipedia.org/wiki/Variance#Properties
-
-    __slots__ = {
-        '_mu': 'Arithmetic mean of a normal distribution',
-        '_sigma': 'Standard deviation of a normal distribution',
-    }
-
-    def __init__(self, mu=0.0, sigma=1.0):
-        "NormalDist where mu is the mean and sigma is the standard deviation."
-        if sigma < 0.0:
-            raise StatisticsError('sigma must be non-negative')
-        self._mu = float(mu)
-        self._sigma = float(sigma)
-
-    @classmethod
-    def from_samples(cls, data):
-        "Make a normal distribution instance from sample data."
-        if not isinstance(data, (list, tuple)):
-            data = list(data)
-        xbar = fmean(data)
-        return cls(xbar, stdev(data, xbar))
-
-    def samples(self, n, *, seed=None):
-        "Generate *n* samples for a given mean and standard deviation."
-        gauss = random.gauss if seed is None else random.Random(seed).gauss
-        mu, sigma = self._mu, self._sigma
-        return [gauss(mu, sigma) for i in range(n)]
-
-    def pdf(self, x):
-        "Probability density function.  P(x <= X < x+dx) / dx"
-        variance = self._sigma ** 2.0
-        if not variance:
-            raise StatisticsError('pdf() not defined when sigma is zero')
-        return exp((x - self._mu)**2.0 / (-2.0*variance)) / sqrt(tau*variance)
-
-    def cdf(self, x):
-        "Cumulative distribution function.  P(X <= x)"
-        if not self._sigma:
-            raise StatisticsError('cdf() not defined when sigma is zero')
-        return 0.5 * (1.0 + erf((x - self._mu) / (self._sigma * sqrt(2.0))))
-
-    def inv_cdf(self, p):
-        """Inverse cumulative distribution function.  x : P(X <= x) = p
+def pdf(self, x):
+    "Probability density function.  P(x <= X < x+dx) / dx"
+    variance = self._sigma ** 2.0
+    if not variance:
+        raise StatisticsError('pdf() not defined when sigma is zero')
+    return exp((x - self._mu)**2.0 / (-2.0*variance)) / sqrt(tau*variance)
+def cdf(self, x):
+    "Cumulative distribution function.  P(X <= x)"
+     if not self._sigma:
+        raise StatisticsError('cdf() not defined when sigma is zero')
+     return 0.5 * (1.0 + erf((x - self._mu) / (self._sigma * sqrt(2.0))))
+def inv_cdf(self, p):
+    """Inverse cumulative distribution function.  x : P(X <= x) = p
         Finds the value of the random variable such that the probability of
         the variable being less than or equal to that value equals the given
         probability.
         This function is also called the percent point function or quantile
         function.
         """
-        if p <= 0.0 or p >= 1.0:
-            raise StatisticsError('p must be in the range 0.0 < p < 1.0')
-        if self._sigma <= 0.0:
-            raise StatisticsError('cdf() not defined when sigma at or below zero')
-        return _normal_dist_inv_cdf(p, self._mu, self._sigma)
+    if p <= 0.0 or p >= 1.0:
+        raise StatisticsError('p must be in the range 0.0 < p < 1.0')
+    if self._sigma <= 0.0:
+        raise StatisticsError('cdf() not defined when sigma at or below zero')
+    return _normal_dist_inv_cdf(p, self._mu, self._sigma)
 
-    def quantiles(self, n=4):
-        """Divide into *n* continuous intervals with equal probability.
-        Returns a list of (n - 1) cut points separating the intervals.
-        Set *n* to 4 for quartiles (the default).  Set *n* to 10 for deciles.
-        Set *n* to 100 for percentiles which gives the 99 cuts points that
-        separate the normal distribution in to 100 equal sized groups.
-        """
-        return [self.inv_cdf(i / n) for i in range(1, n)]
-
-    def overlap(self, other):
-        """Compute the overlapping coefficient (OVL) between two normal distributions.
-        Measures the agreement between two normal probability distributions.
-        Returns a value between 0.0 and 1.0 giving the overlapping area in
-        the two underlying probability density functions.
-            >>> N1 = NormalDist(2.4, 1.6)
-            >>> N2 = NormalDist(3.2, 2.0)
-            >>> N1.overlap(N2)
-            0.8035050657330205
-        """
-        # See: "The overlapping coefficient as a measure of agreement between
-        # probability distributions and point estimation of the overlap of two
-        # normal densities" -- Henry F. Inman and Edwin L. Bradley Jr
-        # http://dx.doi.org/10.1080/03610928908830127
-        if not isinstance(other, NormalDist):
-            raise TypeError('Expected another NormalDist instance')
-        X, Y = self, other
-        if (Y._sigma, Y._mu) < (X._sigma, X._mu):  # sort to assure commutativity
-            X, Y = Y, X
-        X_var, Y_var = X.variance, Y.variance
-        if not X_var or not Y_var:
-            raise StatisticsError('overlap() not defined when sigma is zero')
-        dv = Y_var - X_var
-        dm = fabs(Y._mu - X._mu)
-        if not dv:
-            return 1.0 - erf(dm / (2.0 * X._sigma * sqrt(2.0)))
-        a = X._mu * Y_var - Y._mu * X_var
-        b = X._sigma * Y._sigma * sqrt(dm**2.0 + dv * log(Y_var / X_var))
-        x1 = (a + b) / dv
-        x2 = (a - b) / dv
-        return 1.0 - (fabs(Y.cdf(x1) - X.cdf(x1)) + fabs(Y.cdf(x2) - X.cdf(x2)))
-
-    def zscore(self, x):
-        """Compute the Standard Score.  (x - mean) / stdev
-        Describes *x* in terms of the number of standard deviations
-        above or below the mean of the normal distribution.
-        """
-        # https://www.statisticshowto.com/probability-and-statistics/z-score/
-        if not self._sigma:
-            raise StatisticsError('zscore() not defined when sigma is zero')
-        return (x - self._mu) / self._sigma
-
-    @property
-    def mean(self):
-        "Arithmetic mean of the normal distribution."
-        return self._mu
-
-    @property
-    def median(self):
-        "Return the median of the normal distribution"
-        return self._mu
-
-    @property
-    def mode(self):
-        """Return the mode of the normal distribution
-        The mode is the value x where which the probability density
-        function (pdf) takes its maximum value.
-        """
-        return self._mu
-
-    @property
-    def stdev(self):
-        "Standard deviation of the normal distribution."
-        return self._sigma
-
-    @property
-    def variance(self):
-        "Square of the standard deviation."
-        return self._sigma ** 2.0
-
-    def __add__(x1, x2):
-        """Add a constant or another NormalDist instance.
-        If *other* is a constant, translate mu by the constant,
-        leaving sigma unchanged.
-        If *other* is a NormalDist, add both the means and the variances.
-        Mathematically, this works only if the two distributions are
-        independent or if they are jointly normally distributed.
-        """
-        if isinstance(x2, NormalDist):
-            return NormalDist(x1._mu + x2._mu, hypot(x1._sigma, x2._sigma))
-        return NormalDist(x1._mu + x2, x1._sigma)
-
-    def __sub__(x1, x2):
-        """Subtract a constant or another NormalDist instance.
-        If *other* is a constant, translate by the constant mu,
-        leaving sigma unchanged.
-        If *other* is a NormalDist, subtract the means and add the variances.
-        Mathematically, this works only if the two distributions are
-        independent or if they are jointly normally distributed.
-        """
-        if isinstance(x2, NormalDist):
-            return NormalDist(x1._mu - x2._mu, hypot(x1._sigma, x2._sigma))
-        return NormalDist(x1._mu - x2, x1._sigma)
-
-    def __mul__(x1, x2):
-        """Multiply both mu and sigma by a constant.
-        Used for rescaling, perhaps to change measurement units.
-        Sigma is scaled with the absolute value of the constant.
-        """
-        return NormalDist(x1._mu * x2, x1._sigma * fabs(x2))
-
-    def __truediv__(x1, x2):
-        """Divide both mu and sigma by a constant.
-        Used for rescaling, perhaps to change measurement units.
-        Sigma is scaled with the absolute value of the constant.
-        """
-        return NormalDist(x1._mu / x2, x1._sigma / fabs(x2))
-
-    def __pos__(x1):
-        "Return a copy of the instance."
-        return NormalDist(x1._mu, x1._sigma)
-
-    def __neg__(x1):
-        "Negates mu while keeping sigma the same."
-        return NormalDist(-x1._mu, x1._sigma)
-
-    __radd__ = __add__
-
-    def __rsub__(x1, x2):
-        "Subtract a NormalDist from a constant or another NormalDist."
-        return -(x1 - x2)
-
-    __rmul__ = __mul__
-
-    def __eq__(x1, x2):
-        "Two NormalDist objects are equal if their mu and sigma are both equal."
-        if not isinstance(x2, NormalDist):
-            return NotImplemented
-        return x1._mu == x2._mu and x1._sigma == x2._sigma
-
-    def __hash__(self):
-        "NormalDist objects hash equal if their mu and sigma are both equal."
-        return hash((self._mu, self._sigma))
-
-    def __repr__(self):
-        return f'{type(self).__name__}(mu={self._mu!r}, sigma={self._sigma!r})'
+    
