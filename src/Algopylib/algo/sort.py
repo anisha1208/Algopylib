a65f4f88475ded96ae92ad1512f384f3d1148133
Dhaval Kumar <dhavalkumar741@gmail.com>
2021-06-21 17:31:13 +0530

    added  merge sort, heap sort and quick sort to sort.py

--- src/Algopylib/algo/sort.py
+++ src/Algopylib/algo/sort.py
@@ -1,4 +1,5 @@
 from typing import List, Union
+from random import randint
 
 def bubble_sort(arr : List , simulation : bool = False) -> List:
     """Sorts A List using bubble sort algorithm
@@ -76,3 +77,155 @@ def insertion_sort(arr : List , simulation : bool = False) -> List:
                 print("iteration",iteration,":",*arr)
 
     return arr
+
+def merge_sub(a : List[int], b : List[int]) -> List[int]:
+    """A subroutine of merge sort 
+    Worst-case performance: O(N)
+
+    Parameters:
+        a(List) : Unsorted List  
+        b(List) : Unsorted List
+
+    Returns:
+        c(List) : Two pointer sorted List
+
+    """
+    n = len(a)
+    m = len(b)
+    i, j, k = 0, 0, 0
+    c = [ 0 for i in range(n+m)]
+
+    while(i < n or j < m):
+        if(j == m or (i < n and a[i] < b[j])):
+            c[k] = a[i]
+            k += 1
+            i += 1
+        else:
+            c[k] = b[j]
+            k += 1
+            j += 1
+    
+    return c
+
+def merge_sort(a : List[int]) -> List[int]:
+    """Sorts A List using merge sort algorithm
+    https://en.wikipedia.org/wiki/Merge_sort
+    Worst-case performance: O(Nlog(N))
+
+    Parameters:
+        a(List) : Unsorted List  
+
+    Returns:
+        a(List) : Returns sorted List
+
+    """
+    n = len(a)
+
+    if(n < 2):
+        return a
+    
+    b = [ 0 for i in range(n//2)]
+    c = [ 0 for i in range(n - n//2)]
+
+    for i in range(n):
+        if (i < n//2):
+            b[i] = a[i]
+        else:
+            c[i-(n//2)] = a[i]
+    
+    return merge_sub(merge_sort(b), merge_sort(c)) 
+
+def insert_heap(h : List[int], x : int) -> None:
+    """Inserting an element to heap 
+    https://en.wikipedia.org/wiki/Heap_(data_structure)
+    Worst-case performance: O(log(N))
+
+    Parameters:
+        h(List) : List that represents heap
+        x(int) : element to be inserted  
+
+    """
+    h.append(x)
+    n = len(h)
+    i = n-1
+
+    while(i > 0 and h[i] < h[(i-1)//2]):
+        h[i], h[(i-1)//2] = h[(i-1)//2], h[i]
+        i = (i-1)//2
+
+def remove_min(h : List[int]) -> int:
+    """Removing an element from heap
+    https://en.wikipedia.org/wiki/Heap_(data_structure)
+    Worst-case performance: O(log(N))
+
+    Parameters:
+        h(List) : List that represents heap  
+
+    Returns:
+        last_element(int) : Returns the smallest element of the heap
+
+    """
+    n = len(h)
+    last_element = h[0]
+
+    h[0], h[n-1] = h[n-1], h[0]
+    h.pop()
+    n = len(h)
+
+    i, j = 0, 0
+
+    while(2*i+1 < n):
+        j = 2*i+1
+        if((2*i+2 < n) and h[2*i+2] < h[j]):
+            j = 2*i+2
+        if(h[i] <= h[j]):
+            break
+        h[i], h[j] = h[j], h[i]
+        i = j
+    
+    return last_element
+
+def heap_sort(a : List[int]) -> None:
+    """Sorts A List using heap sort algorithm
+    https://en.wikipedia.org/wiki/Heapsort
+    Worst-case performance: O(Nlog(N))
+
+    Parameters:
+        a(List) : Unsorted List  
+
+    """
+    temp = []
+
+    for i in a:
+        insert_heap(temp, i)
+    
+    for i in range(len(a)):
+        a[i] = remove_min(temp)
+
+def quick_sort(a : List[int], l : int, r : int) -> None:
+    """Sorts A List using quick sort algorithm
+    https://en.wikipedia.org/wiki/Quicksort
+    Worst-case performance: O(N^2)
+    Average performance: O(Nlog(N))
+
+    Parameters:
+        arr(List) : Unsorted List
+        l(int) : left index (present in List) 
+        r(int) : Right index (not present in List) 
+
+    """
+    if(r - l <= 1):
+        return None
+    
+    idx = randint(l, r-1)
+
+    x = a[idx]
+    m = l
+
+    for i in range(l, r, 1):
+        if (a[i] < x):
+            a[i], a[m] = a[m], a[i]
+            m += 1
+
+    quick_sort(a, l, m)
+    quick_sort(a, m, r)
